746575106cd1bc57b9d392a0d5e07a019b19ed4f
LIANLIQ01\Richard <ric_clifford@hotmail.com>
2019-05-09 20:50:05 +0200

    29 buttons and form input for time

--- 29 countdown timer/scripts.js
+++ 29 countdown timer/scripts.js
@@ -1,8 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector(".display__time-left");
 const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
 
 function timer(seconds) {
+  // clear any existing timer
+  clearInterval(countdown);
+
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
@@ -47,3 +51,20 @@ function displayEndTime(timestamp) {
     useGrouping: false
   })}`;
 }
+
+function startTimer()
+{
+  // let's get the data
+   const seconds = parseInt(this.dataset.time);   
+   timer(seconds);  
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer))
+//if element has a name we can just use it in the . bit :)
+document.customForm.addEventListener('submit', function(e) {
+e.preventDefault();
+const mins = this.minutes.value;
+console.log(mins);
+timer(mins * 60);
+this.reset();
+});
